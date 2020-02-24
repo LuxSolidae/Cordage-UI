3b4e9e970ca25b8649fb0f42edb82084cf98a90d
Daniel <daniel.ortizp91@gmail.com>
2020-02-24 13:01:57 -0600

    Improved a little bit the skeleton

---
+++ src/lib/js/atoms/avatar/Avatar.jsx
@@ -0,0 +1,19 @@
+// @flow
+import * as React from 'react';
+import classnames from 'classnames';
+
+export type Props = {
+
+};
+
+const Avatar = (props: Props) => {
+  return (
+    <div></div>
+  );
+}
+
+Avatar.defaultProps = {
+
+};
+
+export default Avatar;
---
+++ src/lib/js/organisms/index.js
@@ -0,0 +1,6 @@
+export * from './drawer';
+export * from './modal';
+export * from './navbar';
+export * from './sidebar';
+export * from './table';
+export * from './tab';
