3eaa8206155fb6ef2955d43770c8194d50881409
Daniel <daniel.ortizp91@gmail.com>
2020-02-25 18:18:38 -0600

    Fixes for gatsby and webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -290,7 +290,7 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
-        'cordage_ui': path.resolve(__dirname, '../src/js/lib/'),
+        '@cordage_ui': path.resolve(__dirname, '../src/js/lib/'),
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
           'react-dom$': 'react-dom/profiling',
