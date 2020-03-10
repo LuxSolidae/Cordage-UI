afe40e844a6d4348b9f6605dd4182f526504b8c2
Óscar A. Montiel <etherealflux94@gmail.com>
2020-03-10 12:01:29 -0600

    Adds infoType inside modal.

--- src/lib/js/organisms/modal/Modal.jsx
+++ src/lib/js/organisms/modal/Modal.jsx
@@ -1,10 +1,18 @@
 // @flow
+
 import * as React from 'react';
 import className from 'classnames';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
-export const Modalc = ({header, info}) => {
+
+export type Props = {
+  header: String,
+  infoType: String,
+  info: String
+}
+
+const Modalc = ({header, infoType, info}) => {
   const [show, setShow] = React.useState(false);
 
   const handleClose = () => setShow(false);
@@ -12,15 +20,24 @@ export const Modalc = ({header, info}) => {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
+      <Button variant='primary' onClick={handleShow}>
         Open Modal
       </Button>
 
-      <Modal className='modal' centered show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
+      <Modal
+          className='modal'
+          keyboard
+          centered
+          show={show}
+          onHide={handleClose}
+        >
+        <Modal.Header className='modal-header' closeButton>
           <Modal.Title>{header}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{info}</Modal.Body>
+        <Modal.Body className='modal-body'>
+          <h3 className='modal-infotype'>{infoType}</h3>
+          {info}
+        </Modal.Body>
       </Modal>
     </>
   );
