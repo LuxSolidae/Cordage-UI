4df0f9e3dbd15f178836bc6b19ccf4bdcec6a193
Óscar A. Montiel <etherealflux94@gmail.com>
2020-03-11 17:14:32 -0600

    Adds modal types props.

--- src/lib/js/organisms/modal/Modal.jsx
+++ src/lib/js/organisms/modal/Modal.jsx
@@ -1,18 +1,22 @@
 // @flow
-
-import * as React from 'react';
-import className from 'classnames';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-
+import * as React from 'react';
 
 export type Props = {
-  header: String,
-  infoType: String,
-  info: String
-}
+  /** Header of the modal */
+  header: string,
+  /** Type of modal to be used (Success, Error, etc.) */
+  modalType?: 'success' | 'error' | 'confirm' |'danger',
+  /** Text inside the modal. */
+  info: string,
+  /** Text inside the button. */
+  buttonText: string,
 
-const Modalc = ({header, infoType, info}) => {
+};
+
+const Modalc = ( {header, modalType, info, buttonText}: Props ) => {
+
   const [show, setShow] = React.useState(false);
 
   const handleClose = () => setShow(false);
@@ -21,22 +25,22 @@ const Modalc = ({header, infoType, info}) => {
   return (
     <>
       <Button variant='primary' onClick={handleShow}>
-        Open Modal
+        {buttonText}
       </Button>
 
       <Modal
-          className='modal'
-          dialogClassName='modal-container'
-          keyboard
-          centered
-          show={show}
-          onHide={handleClose}
-        >
+        className='modal'
+        dialogClassName='modal-container'
+        keyboard
+        centered
+        show={show}
+        onHide={handleClose}
+      >
         <Modal.Header className='modal-header' closeButton>
           <Modal.Title>{header}</Modal.Title>
         </Modal.Header>
         <Modal.Body className='modal-body'>
-          <h3 className='modal-infotype'>{infoType}</h3>
+          <h3 className='modal-infotype'>Success</h3>
           {info}
         </Modal.Body>
       </Modal>
