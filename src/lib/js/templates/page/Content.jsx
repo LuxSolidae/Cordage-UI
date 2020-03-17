1db734f1accdf020e95849774bc2b1df9b2993ca
Daniel Franco <danielfranco9132@gmail.com>
2020-03-17 17:20:41 -0600

    Add title prop to Content component

--- src/lib/js/templates/page/Content.jsx
+++ src/lib/js/templates/page/Content.jsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
 
 type Props = {
-  children: React.Node;
-  className?: string;
+  children: React.Node,
+  title?: string,
+  className?: string,
 }
 
-export const Content = ({ children, className }: Props) => (
+export const Content = ({ children, className, title }: Props) => (
   <div className={`page-content ${className}`}>
+    {
+      title && (
+        <h2 className='content-title'>{title}</h2>
+      )
+    }
     {children}
   </div>
 );
 
 Content.defaultProps = {
   className: '',
+  title: undefined
 };
 
 export default Content;
