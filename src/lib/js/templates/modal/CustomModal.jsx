a379e611641f53ddc6ce56a5a34928afaf2977ab
Óscar A. Montiel <etherealflux94@gmail.com>
2020-03-18 13:07:46 -0600

    Adds deleted Modal files after merge and recolates inside Templates folder.

---
+++ src/lib/js/templates/modal/CustomModal.jsx
@@ -0,0 +1,54 @@
+// @flow
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import * as React from 'react';
+
+export type Props = {
+  /** Header of the modal */
+  header: string,
+  /** Type of modal to be used (Success, Error, etc.) */
+  variant?: 'success' | 'error' | 'confirm' |'danger',
+  /** Text inside the modal. */
+  info: string,
+  /** Text inside the button. */
+  buttonText: string,
+};
+
+const CustomModal = ({header, variant, info, buttonText}: Props) => {
+
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant={variant} onClick={handleShow}>
+        {buttonText}
+      </Button>
+
+      <Modal
+        className='modal'
+        dialogClassName='modal-container'
+        keyboard
+        centered
+        show={show}
+        onHide={handleClose}
+      >
+        <Modal.Header className='modal-header' closeButton>
+          <Modal.Title>{header}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className='modal-body'>
+          <h3 className='modal-infotype'>Success</h3>
+          {info}
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+CustomModal.defaultProps = {
+  variant: 'success',
+};
+
+export default CustomModal;
