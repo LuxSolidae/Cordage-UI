39946f6f43ce247b18dac0f1346dd28ce9e0c64b
Óscar A. Montiel <etherealflux94@gmail.com>
2020-03-23 17:40:48 -0600

    Adds basic props for small card (wip).

--- src/lib/js/molecules/card/Card.jsx
+++ src/lib/js/molecules/card/Card.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import {Card, Button} from 'react-bootstrap';
 
+export type Props = {
+  title: String,
+  content: String,
+  subcontent: String,
+};
 
-const CardComponent = () => {
+const CardComponent = (props: Props) => {
+
+  const {
+    title,
+    content,
+    subcontent
+  } = props;
 
   return(
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top"/>
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+  <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top"/>
+      <Card.Body>
+      <Card.Title>{title}</Card.Title>
+      <Card.Text>
+        {content}
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
   )
 }
 
