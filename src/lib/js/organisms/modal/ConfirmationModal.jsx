7bb47dc514cd49ce4e3d333dadad41f1e988325f
Óscar A. Montiel <etherealflux94@gmail.com>
2020-03-25 12:32:25 -0600

    Adds stable styling and operation for Confirmation Modal (wip).

---
+++ src/lib/js/organisms/modal/ConfirmationModal.jsx
@@ -0,0 +1,49 @@
+// @flow
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import classnames from 'classnames';
+import * as React from 'react';
+
+export type Props = {
+  /** Header of the modal */
+  header: string,
+  /** Text inside the modal. */
+  content: string,
+  /** Text inside the button. */
+  btnConfirm: string,
+};
+
+export const ConfirmationModal = ({ header, content, btnConfirm }: Props) => {
+
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Click to open Confirmation Modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose} animation={true} centered>
+        <div className='modal-confirm'>
+          <Modal.Header closeButton>
+            <Modal.Title>{ header }</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className='modal-confirm-body'>{ content }</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Yes, confirm
+            </Button>
+          </Modal.Footer>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default ConfirmationModal;
