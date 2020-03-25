6314c65d067575ca7fc0c4fbeffd36257a9cf0f9
Óscar A. Montiel <etherealflux94@gmail.com>
2020-03-25 10:25:43 -0600

    Adds a lot of styling and collapsable functionality to Card Extended.

--- src/lib/js/molecules/card/CardExtended.jsx
+++ src/lib/js/molecules/card/CardExtended.jsx
@@ -1,38 +1,50 @@
 import React from 'react';
 import { Accordion, Card, Button, Container, Col } from 'react-bootstrap';
+import points from '@_src_/lib/img/icons/more-points.svg';
 import classnames from 'classnames';
 
 export type Props = {
-  title: String,
-  image: String,
-  content: String,
-  subcontent: String,
+  jobTitle:  String,
+  jobPlace:  String,
+  jobDescription: String,
+  seniorityLevel: String,
+  industry: String,
+  employmentType: String,
+  jobFunctions: String
 };
 
 const CardExtended = (props: Props) => {
 
   const {
-    title,
-    image,
-    subtitle,
+    jobTitle,
+    jobPlace,
+    jobDescription,
+    seniorityLevel,
+    industry,
+    employmentType,
+    jobFunctions
   } = props;
 
   return(
     <Accordion  defaultActiveKey="0">
     <Card>
       <Card.Header className='card-extended'>
-        <Container className='d-flex'>
-          <Col xs={10}>
-            cosas
-          </Col>
-          <Col xs={2}>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              ...
-            </Accordion.Toggle>
-          </Col>
-        </Container>
+        <div className='card-ext-header-wrapper'>
+        <div className='d-flex'>
+          <img className='card-extended-image' src='https://images.dog.ceo/breeds/clumber/n02101556_6883.jpg' alt='profile image' />
+          <div>
+            <p className='card-ext-jobTitle'>{ jobTitle }</p>
+            <p className='card-ext-place'>{ jobPlace }</p>
+          </div>
+        </div>
+          <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            <img src={points} alt='Expand' />
+          </Accordion.Toggle>
+        </div>
         <div>
-          <p className='card-extended-info'>Mid-weight/senior designer to create status-quo-challenging design work across a wide range of media for marketing efforts, including large scale, high profile branded conferences and events, product launch campaigns, presentations, web content, ads, videos, infographics, DM and marketing material.</p>
+          <p className='card-extended-info'>
+            { jobDescription }
+          </p>
         </div>
       </Card.Header>
       <Accordion.Collapse eventKey="0">
@@ -40,19 +52,19 @@ const CardExtended = (props: Props) => {
         <Container className='d-flex'>
           <Col>
             <p className='card-extended-titles'>Seniority Level</p>
-            <p className='card-extended-subtitles'>Not applicable</p>
+            <p className='card-extended-subtitles'>{ seniorityLevel }</p>
           </Col>
           <Col>
             <p className='card-extended-titles'>Indusrty</p>
-            <p className='card-extended-subtitles'>Financial Services</p>
+            <p className='card-extended-subtitles'>{ industry }</p>
           </Col>
           <Col>
             <p className='card-extended-titles'>Employment Type</p>
-            <p className='card-extended-subtitles'>Temporary</p>
+            <p className='card-extended-subtitles'>{ employmentType }</p>
           </Col>
           <Col>
             <p className='card-extended-titles'>Job Functions</p>
-            <p className='card-extended-subtitles'>Marketing</p>
+            <p className='card-extended-subtitles'>{ jobFunctions }</p>
           </Col>
           <Col>
             <p className='card-extended-titles'>Seniority Level</p>
