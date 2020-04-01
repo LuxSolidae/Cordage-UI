701dfe65a7739d4752081932b49fa10bc726ac86
Alan M <alan.maldonado.lara1@gmail.com>
2020-03-31 21:21:44 -0600

    Rework of Input docz and Select functionality

---
+++ src/lib/js/atoms/inputs/Helpers.js
@@ -0,0 +1,24 @@
+import * as Yup from 'yup';
+
+export const Schema = Yup.object().shape({
+  text: Yup.string()
+    .min(3, "Text must have at least 3 characters.")
+    .max(50, "Text can't be longer than 50 characters.")
+    .required("Text is required."),
+  email: Yup.string()
+    .email("Enter a valid email.")
+    .required("Email is required."),
+  phone: Yup.string()
+    .min(8, "Phone must have at least 8 numbers.")
+    .max(12, "Phone can' be longer than 12 characters."),
+  select: Yup.string()
+    .ensure()
+    .required("Select is required.")
+});
+
+export const options = [
+  { label: 'Option 1', value: 1 },
+  { label: 'Option 2', value: 2 },
+  { label: 'Option 3', value: 3 },
+];
+
