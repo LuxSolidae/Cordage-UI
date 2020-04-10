646df781f43576c15d4aacc38c0f04c809394790
Óscar A. Montiel <etherealflux94@gmail.com>
2020-04-10 13:29:27 -0500

    Adds customizible (style) checkbox using react-bootstrap (wip).

--- src/lib/js/atoms/inputs/Checkbox.jsx
+++ src/lib/js/atoms/inputs/Checkbox.jsx
@@ -4,30 +4,39 @@ import _ from 'lodash';
 import { Form, FormCheck } from "react-bootstrap";
 import { ErrorMessage } from "formik";
 
-export const Checkbox = () => {
-
-  return(
-    <div>
-    <Form>
-    {['checkbox', 'radio'].map((type) => (
-      <div key={`default-${type}`} className="mb-3">
-        <Form.Check
-          type={type}
-          id={`default-${type}`}
-          label={`default ${type}`}
-        />
-
-        <Form.Check
-          disabled
-          type={type}
-          label={`disabled ${type}`}
-          id={`disabled-default-${type}`}
-        />
-      </div>
-    ))}
-  </Form>
-    </div>
-  );
+export type Props = {
+  isChecked: Boolean,
+  changeHandler: Function
 }
+
+export const Checkbox = ({
+  isChecked,
+  changeHandler
+}: Props) => {
+
+
+  return (
+    <Form>
+  {['checkbox', 'radio'].map((type) => (
+    <div key={`custom-${type}`} className="mb-3">
+      <Form.Check
+        custom
+        type={type}
+        id={`custom-${type}`}
+        label={`Check this custom ${type}`}
+      />
+
+      <Form.Check
+        custom
+        disabled
+        type={type}
+        label={`disabled ${type}`}
+        id={`disabled-custom-${type}`}
+      />
+    </div>
+  ))}
+</Form>
+  );
+};
 
 export default Checkbox;
