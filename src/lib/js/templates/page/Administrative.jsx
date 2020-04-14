ed14e9ff87b51fd39d1560e1e6168103c3b54efe
Óscar A. Montiel <etherealflux94@gmail.com>
2020-04-14 15:06:50 -0500

    Adds new prop (headerComponent) for adding extra components in position specified in the design.

--- src/lib/js/templates/page/Administrative.jsx
+++ src/lib/js/templates/page/Administrative.jsx
@@ -5,23 +5,36 @@ import { Grid } from './../../atoms';
 type Props = {
   children: React.Node,
   title: string,
+  /**May add any component next to the header. Should be inside a fragment. */
+  headerComponents?: React.Node,
   description?: string,
 };
 
-export const Administrative = ({ children, title, description }: Props) => (
+export const Administrative = ({ children, title, description, headerComponents }: Props) => (
   <div className='page page-administrative'>
     <div className='page-header'>
       <Grid.Container className='pg-container'>
         <Grid.Row>
           <Grid.Col>
-            <h1>{title}</h1>
-            {
-              description && (
-                <p>{description}</p>
-              )
-            }
+            <div>
+              <div className='page-admin-header'>
+                <h1>{title}</h1>
+                {
+                  description && (
+                    <p>{description}</p>
+                  )
+                }
+            </div>
+          </div>
           </Grid.Col>
           <Grid.Col>
+          {
+            headerComponents && (
+              <div className='page-admin-header-extra'>
+                  <span>{headerComponents}</span>
+              </div>
+              )
+          }
           </Grid.Col>
         </Grid.Row>
       </Grid.Container>
