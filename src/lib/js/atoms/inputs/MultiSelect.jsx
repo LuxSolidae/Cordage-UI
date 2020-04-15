3223bf603a70d350929961a2c1876c1633649efe
Alan M <alan.maldonado.lara1@gmail.com>
2020-04-14 21:32:56 -0500

    Rework custom option component integration.

--- src/lib/js/atoms/inputs/MultiSelect.jsx
+++ src/lib/js/atoms/inputs/MultiSelect.jsx
@@ -1,57 +1,60 @@
 // @flow
-import * as React from 'react';
-import { FormGroup, FormLabel } from 'react-bootstrap';
-import Select, { components } from 'react-select';
+import * as React from "react";
+import { FormGroup, FormLabel } from "react-bootstrap";
+import Select, { components } from "react-select";
 import close from "../../../img/icons/close-white.svg";
 
+export const MultiSelect = (props) => {
+  const { label, options, defaultValue } = props;
+
+  return (
+    <FormGroup>
+      {label && <FormLabel>{label}</FormLabel>}
+
+      <Select
+        classNamePrefix="multi-select"
+        options={options}
+        components={{ Option: Option, MultiValueLabel, MultiValueRemove }}
+        defaultValue={defaultValue}
+        isMulti
+        menuIsOpen={true}
+      />
+    </FormGroup>
+  );
+};
+
+const Option = (props) => {
+  return (
+    <components.Option {...props} className="custom-option">
+      {props.data.img && (
+        <img
+          className="custom-option__img"
+          src={props.data.img}
+          alt={props.data.label}
+        />
+      )}
+      <span className="custom-option__label">{props.data.label}</span>
+      {props.data.auxText && (
+        <span className="custom-option__aux-text">{props.data.auxText}</span>
+      )}
+    </components.Option>
+  );
+};
 
 const MultiValueRemove = (props) => {
   return (
     <components.MultiValueRemove {...props}>
-      <img src={close} alt='Close'></img>
+      <img src={close} alt="Close"></img>
     </components.MultiValueRemove>
   );
-}
-
-export const MultiSelect = (props: Props) => {
-  const {
-    label,
-    options,
-    defaultValue
-  } = props;
-
-
-  return (
-    <FormGroup>
-      { label && <FormLabel>{ label }</FormLabel>}
-
-      <Select
-        classNamePrefix='multi-select'
-        options={options}
-        formatOptionLabel={CustomOption}
-        components={{MultiValueLabel, MultiValueRemove}}
-        defaultValue={defaultValue}
-        isMulti
-      />
-    </FormGroup>
-  );
-}
-
-const CustomOption = ({ value, label, auxText, img }) => (
-  <div className='custom-option'>
-    { img && <img className='custom-option__img' src={img} alt={label}/> }
-
-    <span className='custom-option__label'>{ label }</span>
-    { auxText && <span className='custom-option__aux-text'>{ auxText }</span> }
-  </div>
-);
+};
 
 const MultiValueLabel = (props) => {
   return (
     <components.MultiValueLabel {...props}>
-      { props.data.label }
+      {props.data.label}
     </components.MultiValueLabel>
   );
-}
+};
 
 export default MultiSelect;
