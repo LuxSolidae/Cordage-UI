d4bce6dbcc66891fef983f0ff435d8342431d31a
Alan M <alan.maldonado.lara1@gmail.com>
2020-04-17 15:30:34 -0500

    Create component skeleton.

---
+++ src/lib/js/atoms/inputs/Switch.jsx
@@ -0,0 +1,19 @@
+// @flow
+import * as React from "react";
+
+
+export type Props = {};
+
+export const Switch = (props: Props) => {
+  const {} = props;
+
+  return (
+    <>
+      <span>Switch component</span>
+    </>
+  );
+};
+
+Switch.defaultProps = {};
+
+export default Switch;
--- src/lib/js/atoms/inputs/index.js
+++ src/lib/js/atoms/inputs/index.js
@@ -1,9 +1,11 @@
-import Select from './Select';
-import TextField from './TextField';
 import MultiSelect from './MultiSelect';
+import Select from './Select';
+import Switch from './Switch';
+import TextField from './TextField';
 
 export {
   MultiSelect,
   Select,
+  Switch,
   TextField,
 };
